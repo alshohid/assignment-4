23f653afc1c5b10629456db49aae5308e5dca355
alshohid <alshohid1997@gmail.com>
2023-07-07 22:05:06 +0600

    Adding all file

---
+++ productDisplay.js
@@ -0,0 +1,160 @@
+const openShopping= document.querySelector('.shopping');
+const closeShopping= document.querySelector('.closeShopping');
+const body = document.querySelector('body')
+ 
+const total = document.querySelector('.total');
+const list = document.querySelector('.list');
+const listCard = document.querySelector('.listCard');
+
+openShopping.addEventListener('click',()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click',()=>{
+    body.classList.remove('active');
+})
+let products =[
+    {
+        id:1,
+        name:'PRODUCT NAME-1',
+        price:1000
+    },
+    {
+        id:2,
+        name:'PRODUCT NAME-2',
+        price:2000
+    },
+      {
+        id:3,
+        name:'PRODUCT NAME-3',
+        price:3000
+    },
+    {
+        id:4,
+        name:'PRODUCT NAME-4',
+        price:4000
+    },
+      {
+        id:5,
+        name:'PRODUCT NAME-5',
+        price:5000
+    },
+    {
+        id:6,
+        name:'PRODUCT NAME-6',
+        price:6000
+    },
+     {
+        id:7,
+        name:'PRODUCT NAME-7',
+        price:7000
+    },
+    {
+        id:8,
+        name:'PRODUCT NAME-8',
+        price:8000
+    },
+      {
+        id:9,
+        name:'PRODUCT NAME-9',
+        price:9000
+    },
+    {
+        id:10,
+        name:'PRODUCT NAME-10',
+        price:10000
+    },
+ 
+]
+let cardList=[];
+ const displayProduct =()=>{
+    products.forEach((product,key)=>{
+
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('box-Shadow')
+        newDiv.innerHTML=`
+             <h4>${product.id}</h4>
+             <h6>${product.name}</h6>
+             <h4>${product.price.toLocaleString()} <span class='bdt'>BDT </span> </h4>
+             <button class="addToCartButton" onclick="addToCart(${key})"> Add To Cart</button>
+            `
+            list.appendChild(newDiv)
+
+    })
+
+    
+ }
+ displayProduct();
+ function addToCart(key){
+    if(cardList[key] == null){
+        cardList[key]=products[key]
+        cardList[key].quantity=1;
+    }
+    reloadCard()
+ }
+ function reloadCard(){
+    listCard.innerHTML= ''
+    let count =0
+    let totalPrice=0;
+           
+    cardList.forEach((product,key)=>{
+        totalPrice += product.price;
+        count += product.quantity
+   
+    total.innerText= ` Total Price = ${totalPrice.toLocaleString()}`;
+        document.querySelector('.quantity').innerText=count;
+
+        console.log(product);
+        if(product !=null){
+          let newProductList= document.createElement('li')
+          newProductList.classList.add('cart-styling')
+          newProductList.innerHTML +=`
+          <div class='cart-product'>  
+             <div >
+              
+              <h6> ${product.name} </h6>
+              <h6> ${product.quantity} </h6>
+              <h4 class='bdt'> ${product.price.toLocaleString()} BDT </h4>
+
+             </div>
+              </div>
+             <div class='myRemoveAndBuy'> 
+             <div > 
+                
+                
+              <button class='updateProductBtn' onclick='updateHandler(${key},${product.quantity -1})'>Remove</button>
+              
+             </div>
+              
+              <div>
+              <button class='buyBtn' onclick='buyProduct(${key})'> Buy Now </button>
+             </div>
+             
+             
+             </div>
+             
+            
+          
+          `
+          listCard.appendChild(newProductList)
+        }
+       
+    })
+  
+ }
+ function changeQuantity(key, qty){
+            console.log(key, qty);
+ }
+ 
+ function buyProduct(){
+
+ }
+ function updateHandler(key,quantity){
+  if(quantity == 0){
+    delete cardList[key];
+  }
+  else{
+    cardList[key].quantity = quantity;
+    cardList[key].price = quantity* products[key].price;
+  }
+     reloadCard()
+ }
